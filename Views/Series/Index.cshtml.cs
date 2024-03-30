5f3056d45eaea130860e2c09931820f71defd2bd
Roland <114315281+sszemtelen@users.noreply.github.com>
2024-03-30 17:16:59 +0100

    Add files via upload

---
+++ Views/Series/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BulkRename.Pages
+{
+    public class FilesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
