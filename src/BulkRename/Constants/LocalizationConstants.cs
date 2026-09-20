daa5ebb338e2b83c39fe9b2569caec3584510e0c
Ramazan Yilmaz <r.yilmaz@outlook.com>
2026-09-20 17:02:57 +0200

    Make LocalizationConstants also internal for consistency

--- src/BulkRename/Constants/LocalizationConstants.cs
+++ src/BulkRename/Constants/LocalizationConstants.cs
@@ -1,70 +1,70 @@
 ﻿namespace BulkRename.Constants
 {
-    public static class LocalizationConstants
+    internal static class LocalizationConstants
     {
-        public const string AN_ERROR_OCCURED_HEADER = "AnErrorOccuredHeader";
+        internal const string AN_ERROR_OCCURED_HEADER = "AnErrorOccuredHeader";
 
-        public const string ABOUT = "About";
+        internal const string ABOUT = "About";
 
-        public const string BUILD_DATE = "BuildDate";
+        internal const string BUILD_DATE = "BuildDate";
 
-        public const string COMMIT = "Commit";
+        internal const string COMMIT = "Commit";
 
-        public const string COMMIT_HASH = "CommitHash";
+        internal const string COMMIT_HASH = "CommitHash";
 
-        public const string COPYRIGHT = "Copyright";
+        internal const string COPYRIGHT = "Copyright";
 
-        public const string DETAILED_ERROR_MESSAGE = "DetailedErrorMessage";
+        internal const string DETAILED_ERROR_MESSAGE = "DetailedErrorMessage";
 
-        public const string DEVELOPMENT_MODE = "DevelopmentMode";
+        internal const string DEVELOPMENT_MODE = "DevelopmentMode";
 
-        public const string ERROR = "Error";
+        internal const string ERROR = "Error";
 
-        public const string FILE_SIZE_IN_MB = "FileSizeInMb";
+        internal const string FILE_SIZE_IN_MB = "FileSizeInMb";
 
-        public const string HISTORY = "History";
+        internal const string HISTORY = "History";
 
-        public const string HOME = "Home";
+        internal const string HOME = "Home";
 
-        public const string INFORMATIONAL_VERSION = "InformationalVersion";
+        internal const string INFORMATIONAL_VERSION = "InformationalVersion";
 
-        public const string LOAD_HISTORY = "LoadHistory";
+        internal const string LOAD_HISTORY = "LoadHistory";
 
-        public const string NEW_NAME = "NewName";
+        internal const string NEW_NAME = "NewName";
 
-        public const string OLD_NAME = "OldName";
+        internal const string OLD_NAME = "OldName";
 
-        public const string OPEN_COMMIT = "OpenCommit";
+        internal const string OPEN_COMMIT = "OpenCommit";
 
-        public const string OPEN_ISSUES = "OpenIssues";
+        internal const string OPEN_ISSUES = "OpenIssues";
 
-        public const string PREVIEW_RENAMING_OF_TV_SHOWS = "PreviewRenamingOfTvShows";
+        internal const string PREVIEW_RENAMING_OF_TV_SHOWS = "PreviewRenamingOfTvShows";
 
-        public const string RENAMED_ON = "RenamedOn";
+        internal const string RENAMED_ON = "RenamedOn";
 
-        public const string REPORT_A_PROBLEM = "ReportAProblem";
+        internal const string REPORT_A_PROBLEM = "ReportAProblem";
 
-        public const string REPOSITORY = "Repository";
+        internal const string REPOSITORY = "Repository";
 
-        public const string REQUEST = "Request";
+        internal const string REQUEST = "Request";
 
-        public const string SERIES = "Series";
+        internal const string SERIES = "Series";
 
-        public const string SPONSOR_PAGE = "SponsorPage";
+        internal const string SPONSOR_PAGE = "SponsorPage";
 
-        public const string SUBMIT_RENAMING = "Submit Renaming";
+        internal const string SUBMIT_RENAMING = "Submit Renaming";
 
-        public const string SUCCESSFULLY_RENAMED_FILES = "SuccessfullyRenamedFiles";
+        internal const string SUCCESSFULLY_RENAMED_FILES = "SuccessfullyRenamedFiles";
 
-        public const string SUPPORT_THIS_PROJECT = "SupportThisProject";
+        internal const string SUPPORT_THIS_PROJECT = "SupportThisProject";
 
-        public const string SUPPORT_THIS_PROJECT_WITH_HEART = "SupportThisProjectWithHeart";
+        internal const string SUPPORT_THIS_PROJECT_WITH_HEART = "SupportThisProjectWithHeart";
 
-        public const string SWAPPING_TO_DEVELOPMENT_MODE_DISPLAY =
+        internal const string SWAPPING_TO_DEVELOPMENT_MODE_DISPLAY =
             "SwappingToDevelopmentModeDisplay";
 
-        public const string VERSION = "Version";
+        internal const string VERSION = "Version";
 
-        public const string WELCOME_TO_BULK_RENAME = "WelcomeToBulkRename";
+        internal const string WELCOME_TO_BULK_RENAME = "WelcomeToBulkRename";
     }
 }
