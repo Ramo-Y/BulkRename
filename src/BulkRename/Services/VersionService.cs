2fd58a579cab3e83087761fc1f4846ba1761774b
Ramazan Yilmaz <r.yilmaz@outlook.com>
2026-09-22 18:12:37 +0200

    Separate constants for attributes

--- src/BulkRename/Services/VersionService.cs
+++ src/BulkRename/Services/VersionService.cs
@@ -19,18 +19,14 @@ namespace BulkRename.Services
 
         public string GetAuthors()
         {
-            var metadataAttributes = GetMetadataAttributes();
-            var attribute = metadataAttributes?.First(c =>
-                c.Key.Equals(EnvironmentConstants.AUTHORS_ATTRIBUTE)
-            );
+            var attribute = GetMetadataAttribute(AttributeConstants.AUTHORS_ATTRIBUTE);
             var url = attribute?.Value ?? string.Empty;
             return url;
         }
 
         public string GetCommitHash()
         {
-            var metadataAttributes = GetMetadataAttributes();
-            var attribute = metadataAttributes?.First(c => c.Key.Equals("GitCommit"));
+            var attribute = GetMetadataAttribute(AttributeConstants.GIT_COMMIT_ATTRIBUTE);
             var commitHash = attribute?.Value;
 
             if (string.IsNullOrWhiteSpace(commitHash))
@@ -61,8 +57,7 @@ namespace BulkRename.Services
 
         public DateTime GetBuildDate()
         {
-            var metadataAttributes = GetMetadataAttributes();
-            var attribute = metadataAttributes?.First(c => c.Key.Equals("BuildDate"));
+            var attribute = GetMetadataAttribute(AttributeConstants.BUILD_DATE_ATTRIBUTE);
             var dateString = attribute?.Value ?? string.Empty;
             DateTime.TryParseExact(
                 dateString,
@@ -76,20 +71,14 @@ namespace BulkRename.Services
 
         public string GetRepositoryUrl()
         {
-            var metadataAttributes = GetMetadataAttributes();
-            var attribute = metadataAttributes?.First(c =>
-                c.Key.Equals(EnvironmentConstants.REPOSITORY_URL_ATTRIBUTE)
-            );
+            var attribute = GetMetadataAttribute(AttributeConstants.REPOSITORY_URL_ATTRIBUTE);
             var url = attribute?.Value ?? string.Empty;
             return url;
         }
 
         public string GetSupportProjectUrl()
         {
-            var metadataAttributes = GetMetadataAttributes();
-            var attribute = metadataAttributes?.First(c =>
-                c.Key.Equals(EnvironmentConstants.SUPPORT_PROJECT_URL_ATTRIBUTE)
-            );
+            var attribute = GetMetadataAttribute(AttributeConstants.SUPPORT_PROJECT_URL_ATTRIBUTE);
             var url = attribute?.Value ?? string.Empty;
             return url;
         }
@@ -102,11 +91,16 @@ namespace BulkRename.Services
             return copyright;
         }
 
-        private IEnumerable<AssemblyMetadataAttribute> GetMetadataAttributes()
+        private AssemblyMetadataAttribute? GetMetadataAttribute(string attributeName)
         {
             var assembly = GetType().Assembly;
             var metadataAttributes = assembly.GetCustomAttributes<AssemblyMetadataAttribute>();
-            return metadataAttributes;
+            var attribute = metadataAttributes?.First(c =>
+                c.Key.Equals(attributeName)
+            );
+
+            return attribute;
+
         }
     }
 }
